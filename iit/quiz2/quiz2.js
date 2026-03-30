3e34f7784eccb4f62b3e5dd85c9bbc6d02ea307c
D'Andre Collins <dandrecollins07@gmail.com>
2026-03-30 18:56:36 -0400

    Number 1 complete

--- iit/quiz2/quiz2.js
+++ iit/quiz2/quiz2.js
@@ -1,4 +1,28 @@
 // Quiz 2
 // Put your javascript here in a document.ready function
+alert("This page is about to load");
 
+$(document).ready(function () {
 
+    var defaultTitle = "ITWS 1100 - Quiz 2";
+
+    // set default title
+    document.title = defaultTitle;
+
+    $("#goButton").click(function () {
+        if (document.title === defaultTitle) {
+            document.title = "D'Andre Collins - Quiz 2";
+        } else {
+            document.title = defaultTitle;
+        }
+    });
+
+    $(".last-name").hover(
+        function () {
+            $(this).addClass("makeItPurple");
+        },
+        function () {
+            $(this).removeClass("makeItPurple");
+        }
+    );
+});
