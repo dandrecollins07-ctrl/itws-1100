91336fd49dda0330d67e9d0260ee6b0dac60181e
D'Andre Collins <dandrecollins07@gmail.com>
2026-04-16 22:01:18 -0400

    Rerun

--- iit/lab9/actors.php
+++ iit/lab9/actors.php
@@ -1,18 +1,41 @@
 <?php
+// SHOW ERRORS (for debugging)
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-?>
-<?php
-include 'config.php';
 
+// CONNECT TO DATABASE
+$conn = mysqli_connect("localhost", "phpmyadmin", "", "iit");
+
+// CHECK CONNECTION
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// QUERY
 $sql = "SELECT * FROM actors";
 $result = mysqli_query($conn, $sql);
 
+// CHECK QUERY
 if (!$result) {
     die("Query failed: " . mysqli_error($conn));
 }
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Actors</title>
+</head>
+<body>
+
+<h1>Actors List</h1>
+
+<?php
+// DISPLAY DATA
 while ($row = mysqli_fetch_assoc($result)) {
     echo $row['firstname'] . " " . $row['lastname'] . "<br>";
 }
 ?>
+
+</body>
+</html>
